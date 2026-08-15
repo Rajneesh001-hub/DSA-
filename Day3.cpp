702189ce27cb0a77adb1232eebb892748886f7b7
Rajneesh Yadav <rajneesharya055@gmail.com>
2026-08-15 14:31:42 +0530

    Add Day3 C++ program

---
+++ Day3.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+char c ='d';
+// {
+// if(C=='a')
+//     cout<<"Vowel"<<endl;
+// else if(C=='e')
+// cout<<"Vowel"<<endl;
+
+// else if(C=='i')
+
+// cout<<"vowel"<<endl;
+
+// else if(C=='O')
+// cout<<"vowel"<<endl; 
+
+// else
+// cout<<"consonant"<<endl;
+
+//  }
+
+
+if(c =='a' || c=='e' || c=='i'|| c=='o' || c=='u')
+cout<<"Vowel";
+
+else
+cout<<"Consonant";
+
+return 0;
+
+}
+
+
+
+
+
