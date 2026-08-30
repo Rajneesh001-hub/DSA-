29e237ad4ea99547fe645087d3d27cc41046b7dc
Rajneesh Yadav <rajneesharya055@gmail.com>
2026-08-30 22:29:24 +0530

    Add Array DSA solutions

---
+++ Array/largest.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int arr[6]={6,7,3,5,44,6};
+    int largest=arr[0];
+
+    for(int i=1; i<=5; i++){
+
+        if(arr[i]>largest)
+        largest=arr[i];
+
+    
+    }
+    cout<<largest;
+
+    return 0;
+
+}
