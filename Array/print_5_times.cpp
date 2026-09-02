203c786ab71d5582a021d3b9ffa405ce15f093eb
Rajneesh Yadav <rajneesharya055@gmail.com>
2026-09-02 12:55:30 +0530

    print 5 times

---
+++ Array/print_5_times.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int arr[5]={4,8,9,4,5};
+
+    for(int i=0;  i<5; i++)
+    {
+        for (int j=0; j<5 ; j++)
+        cout<<arr[j]<<" ";
+        cout<<endl;
+
+    }
+    return 0;
+}
