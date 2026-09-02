e34d9da65b1739f065aa02af26863c8ffebcab5f
Rajneesh Yadav <rajneesharya055@gmail.com>
2026-09-02 13:46:55 +0530

    dec

---
+++ Array/dec.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int arr[5]={4,8,9,4,5};
+
+    for(int i=0;  i<5; i++){
+
+        for (int j=0; j<5-i ;j++)
+
+        cout<<arr[j]<<" ";
+        cout<<endl;
+
+    }
+        
+    
+    return 0;
+}
